66d22f673f90e1e0a759f259f0234e9aea03d3cb
Simon Wehrli <wehrli.simon@gmail.com>
2012-10-08 13:53:04 +0200

    commit necessary to merge

--- VS_siwehrli_Sensors/gen/ch/ethz/inf/vs/android/siwehrli/sensors/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ch.ethz.inf.vs.android.siwehrli.sensors;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
